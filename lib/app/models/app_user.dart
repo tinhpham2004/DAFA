db2be35fae8f04c84dac3ccd38ec66445b84df50
tinhpham2004 <boydzpro3032004@gmail.com>
2024-01-20 15:51:38 +0700

    validate

--- lib/app/models/app_user.dart
+++ lib/app/models/app_user.dart
@@ -2,9 +2,17 @@ class AppUser {
   String userId;
   String phoneNumber;
   String password;
+  List<String> images;
+  String name;
+  String dateOfBirth;
+  String gender;
   AppUser({
     required this.userId,
     required this.phoneNumber,
     required this.password,
+    required this.images,
+    required this.name,
+    required this.dateOfBirth,
+    required this.gender,
   });
 }
--- lib/app/modules/complete_profile/widgets/finish_button.dart
+++ lib/app/modules/complete_profile/widgets/finish_button.dart
@@ -1,4 +1,5 @@
 import 'package:dafa/app/core/values/app_colors.dart';
+import 'package:dafa/app/models/app_user.dart';
 import 'package:dafa/app/modules/complete_profile/complete_profile_controller.dart';
 import 'package:dafa/app/routes/app_routes.dart';
 import 'package:dafa/app/services/database_service.dart';
@@ -26,19 +27,43 @@ class FinishButton extends StatelessWidget {
       ),
       child: ElevatedButton(
         onPressed: () async {
-          await databaseService.UpdateUserImage(
-              1, completeProfileController.imgDownloadUrl1.value);
-          await databaseService.UpdateUserImage(
-              2, completeProfileController.imgDownloadUrl2.value);
-          await databaseService.UpdateUserImage(
-              3, completeProfileController.imgDownloadUrl3.value);
-          await databaseService.UpdateUserImage(
-              4, completeProfileController.imgDownloadUrl4.value);
-          await databaseService.UpdateUserImage(
-              5, completeProfileController.imgDownloadUrl5.value);
-          await databaseService.UpdateUserImage(
-              6, completeProfileController.imgDownloadUrl6.value);
-          Get.toNamed(AppRoutes.swipe);
+          List<String> images = [
+            completeProfileController.imgDownloadUrl1.value,
+            completeProfileController.imgDownloadUrl2.value,
+            completeProfileController.imgDownloadUrl3.value,
+            completeProfileController.imgDownloadUrl4.value,
+            completeProfileController.imgDownloadUrl5.value,
+            completeProfileController.imgDownloadUrl6.value
+          ];
+          int count = 0;
+          images.forEach((image) {
+            if (image == '') count++;
+          });
+          if (count >= 3) {
+            completeProfileController.UpdateErrorImages(true);
+          } else {
+            String name = completeProfileController.name.text;
+            String dateOfBirth = completeProfileController.dateOB1.text +
+                completeProfileController.dateOB2.text +
+                "/" +
+                completeProfileController.dateOB3.text +
+                completeProfileController.dateOB4.text +
+                "/" +
+                completeProfileController.dateOB5.text +
+                completeProfileController.dateOB6.text +
+                completeProfileController.dateOB7.text +
+                completeProfileController.dateOB8.text;
+            String gender = '';
+            if (completeProfileController.gender.value == 1)
+              gender = "Man";
+            else if (completeProfileController.gender.value == 2)
+              gender = "Woman";
+            else if (completeProfileController.gender.value == 3)
+              gender = "LGBT";
+            await databaseService.UpdateUserData(
+                images, name, dateOfBirth, gender);
+            Get.toNamed(AppRoutes.swipe);
+          }
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.transparent,
