16f22bd02227f7dc365e55b205a381255a907090
tinhpham2004 <boydzpro3032004@gmail.com>
2024-02-08 00:20:38 +0700

    sign in state

--- lib/app/modules/auth/widgets/sign_in_button.dart
+++ lib/app/modules/auth/widgets/sign_in_button.dart
@@ -4,6 +4,7 @@ import 'package:dafa/app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SignInButton extends StatelessWidget {
   const SignInButton({
