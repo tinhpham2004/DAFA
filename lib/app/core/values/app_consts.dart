f9fd23f0d0f731733450bf2c3974e8e325086faa
tinhpham2004 <boydzpro3032004@gmail.com>
2024-03-15 16:39:58 +0700

    update dev

--- lib/app/core/values/app_consts.dart
+++ lib/app/core/values/app_consts.dart
@@ -6,5 +6,6 @@ class AppConsts {
   static const String FCM_Key =
       'AAAAYyCB2zI:APA91bEls1OyO67AI0tFvr167ZL-YgD2EBlBextxSgYalIW-IkFfgHkVadV4jud68mh0xVvycbBBx-LAuzy_WfuviQ7voog9kyJOtuGDtyrUl8AUM-aDWB2R7Yp1wTYitRm80PMS6Ngl';
   static const agoraAppId = '0127a16f300f4d0997ba704e3e24b26a';
-  static const agoraTokenBaseUrl = 'https://agora-node-token-server-snowy.vercel.app';
+  static const agoraTokenBaseUrl =
+      'https://agora-node-token-server-snowy.vercel.app';
 }
