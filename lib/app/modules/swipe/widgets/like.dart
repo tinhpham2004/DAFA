ba7823cf4fa8e78557f4c422c13447452422cd16
tinhpham2004 <boydzpro3032004@gmail.com>
2024-11-23 21:45:25 +0700

    refactor: update Kotlin version and dependencies; improve call screen logic and swipe functionality

--- lib/app/modules/swipe/widgets/like.dart
+++ lib/app/modules/swipe/widgets/like.dart
@@ -2,6 +2,7 @@ import 'package:dafa/app/core/values/app_colors.dart';
 import 'package:dafa/app/modules/sign_in/sign_in_controller.dart';
 import 'package:dafa/app/modules/swipe/swipe_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -18,8 +19,9 @@ class LikeButton extends StatelessWidget {
     return GestureDetector(
       onTap: () {
         if (swipeController.curIndex.value !=
-            signInController.matchList.length - 1)
-          swipeController.cardSwiperController.swipeRight();
+            signInController.matchList.length - 1) {
+          swipeController.cardSwiperController.swipe(CardSwiperDirection.right);
+        }
       },
       child: Container(
         margin: EdgeInsets.only(top: 1150.h),
